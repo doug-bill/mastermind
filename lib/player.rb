315a105de815197516a0bf4fb8a097d5579331f1
doug-bill <65959310+doug-bill@users.noreply.github.com>
2026-06-20 08:18:52 -0300

    refactor - changed the secret code to be a Class
    This makes it possible for the random code to be accessed by the player
    or computer, depending on who is the code creator.
    
    added - the initial pieces for the player with the menu layout.

--- lib/player.rb
+++ lib/player.rb
@@ -1,1 +1,13 @@
-# The player options 
+# The player options
+
+require_relative "secret"
+
+puts "Pick 4 colors using their numbers:
+
+1: ● red 2: ● blue, 3: ● green, 4: ● yellow, 5: ● magenta, 6: ● white,
+
+Enter your guess (e.g. 1 3 2 5):"
+
+input = gets.chomp.chars.map(&:to_i)
+
+p input
--- lib/secret.rb
+++ lib/secret.rb
@@ -2,20 +2,32 @@
 
 require "colorize"
 
-color_map= {
-  1 => "●".colorize(:red),
-  2 => "●".colorize(:blue),
-  3 => "●".colorize(:green),
-  4 => "●".colorize(:yellow),
-  5 => "●".colorize(:magenta),
-  6 => "●".colorize(:white)
-}
+#class to define the secret code of the game.
+class Secret
+  attr_reader :code
 
-colors = [1, 2, 3, 4, 5, 6]
-code = []
+  def initialize
 
-snippet = Array.new(4) { colors.sample }
-code.push(snippet.map { |num| color_map[num] }.join(" "))
+    @color_map = {
+      1 => "●".colorize(:red),
+      2 => "●".colorize(:blue),
+      3 => "●".colorize(:green),
+      4 => "●".colorize(:yellow),
+      5 => "●".colorize(:magenta),
+      6 => "●".colorize(:white)
+    }
 
-p snippet
-puts code
+    @colors = [1, 2, 3, 4, 5, 6]
+    @code = Array.new(4) { @colors.sample }
+
+  end
+
+  def display_code
+    @color_code = @code.map { |num| @color_map[num] }.join(" ")
+  end
+
+end
+
+secret = Secret.new
+p secret.code
+puts secret.display_code
